4a94b0e5a5abcf87e5eb9e7559594095d415ba01
julei <lei_ju@126.com>
2017-11-16 14:44:43 +0800

    修改：     .angular-cli.json 	新文件：   package-lock.json 	修改：     src/app/app-routing.module.ts 	修改：     src/app/app.component.html 	修改：     src/app/app.module.ts 	修改：     src/index.html 	修改：     src/styles.css 	修改：     src/tsconfig.app.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import { CartlistComponent } from "./components/cartlist/cartlist.component";
+import { OrdersComponent } from "./components/orders/orders.component";
+const routes: Routes = [
+//配置路由
+{
+  path:'home',
+  component:HomeComponent
+},
+{
+  path:'cartlist',
+  component:CartlistComponent
 
-const routes: Routes = [];
+},
+{
+  path:'orders',
+  component:OrdersComponent
+
+},
+{
+  path:'**',
+  component:HomeComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { StorageService} from './services/storage.service';
+import { UtilsService } from "./services/utils.service";
+import {HttpClientModule} from '@angular/common/http';
+ 
+import {
+  Ng2BootstrapModule
+}from 'ngx-bootstrap';
+import { MenuBottomComponent } from './components/menu-bottom/menu-bottom.component';
+import { SearchComponent } from './components/search/search.component';
+import { CartlistComponent } from './components/cartlist/cartlist.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProfileComponent } from './components/profile/profile.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBottomComponent,
+    SearchComponent,
+    CartlistComponent,
+    LoginComponent,
+    SignupComponent,
+    OrdersComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProfileComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,   
+    Ng2BootstrapModule.forRoot(),
+    HttpClientModule
+    
   ],
-  providers: [],
+  providers: [
+    StorageService,
+    UtilsService
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
